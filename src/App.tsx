e2ce9aaf8d1b19beda128fe080548c71d5f42398
PrabhjotSodhi <sodhiprabhjot23@gmail.com>
2024-02-17 13:15:47 +1300

    Add prettier and sidebar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-full w-full overflow-hidden">
+      {/* Side bar */}
+      <nav className="flex w-[260px] flex-col justify-between overflow-x-hidden bg-neutral-900 px-3 py-3.5" aria-history="Chat History">
+        <button className="rounded-lg border-2 border-neutral-950 px-1 py-3 text-sm hover:border-neutral-950 hover:bg-neutral-950">+ New Chat</button>
+        <ul className="flex h-full flex-col gap-2 overflow-y-auto py-4 text-sm text-gray-100">
+          <li className="rounded-lg px-4 py-4 hover:cursor-pointer hover:bg-neutral-950">Test One</li>
+        </ul>
+        <div className="flex w-full flex-col items-center border-t-2 pt-4">
+          <p>Made by Prabhjot Sodhi</p>
+        </div>
+      </nav>
+      {/* Chat Window */}
+      <div className="flex h-full flex-col" tabIndex={0}>
+        <div className="h-full flex-1 items-center justify-center">
+          <div></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
